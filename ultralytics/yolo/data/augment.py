2fdb55062f690c3f8b2c017c1aab1db64338ec23
michelebechini <michele.bechini@polimi.it>
2023-07-13 17:25:32 +0200

    Added sunflares and tuned/debugged

--- ultralytics/yolo/data/augment.py
+++ ultralytics/yolo/data/augment.py
@@ -15,7 +15,7 @@ from ..utils.instance import Instances
 from ..utils.metrics import bbox_ioa
 from ..utils.ops import segment2box
 from .utils import polygons2masks, polygons2masks_overlap
-from .custom_noise import Sensor_VIS, sensor_noise
+from .custom_noise import Sensor_VIS, sensor_noise, apply_random_flare
 
 POSE_FLIPLR_INDEX = [0, 2, 1, 4, 3, 6, 5, 8, 7, 10, 9, 12, 11, 14, 13, 16, 15]
 
@@ -535,13 +535,39 @@ class RandomFlip:
         labels['instances'] = instances
         return labels
     
+
+# new class for noise random noises as RandomSunFlare
+class RandomNoises:
+
+    def __init__(self, p=1.0) -> None:
+        
+        assert 0 <= p <= 1.0
+        self.p = p
+
+    def __call__(self, labels):
+        """Resize image and padding for detection, instance segmentation, pose."""
+        img = labels['img']
+        instances = labels.pop('instances')
+        instances.convert_bbox(format='xywh')
+        h, w = img.shape[:2]
+        h = 1 if instances.normalized else h
+        w = 1 if instances.normalized else w
+
+        # Apply RandomFlare Noise
+        if random.random() < self.p:
+            img = apply_random_flare(img)
+        
+        labels['img'] = np.ascontiguousarray(img)
+        labels['instances'] = instances
+        return labels
+
+
 # new class for noise derived from sensor modeling
 class SensorNoise:
 
-    def __init__(self, p=0.5) -> None:
+    def __init__(self, p=1.0) -> None:
         
         assert 0 <= p <= 1.0
-
         self.p = p
         self.sensor = Sensor_VIS(G=20, is_RGB=False)
         
@@ -558,8 +584,19 @@ class SensorNoise:
         # Apply sensor noise
         if random.random() < self.p:
             self.sensor = Sensor_VIS(G=np.random.randint(20, 40), is_RGB=False)
-            self.sensor.noise_parameters(read_mean=np.random.randint(20, 40) * self.sensor.G)
-            img = sensor_noise(image=img, sensor=self.sensor)
+            self.sensor.noise_parameters(read_mean=np.random.randint(20, 40) * self.sensor.G, bloom_thresh=200, row_std=20.)
+            
+            if random.random() < 0.5:
+                bloom = True
+            else:
+                bloom = False
+
+            if random.random() < 0.5:
+                raw_noise = True
+            else:
+                raw_noise = False
+
+            img = sensor_noise(image=img, sensor=self.sensor, use_bloom=bloom, row_noise=raw_noise)
 
         labels['img'] = np.ascontiguousarray(img)
         labels['instances'] = instances
@@ -678,7 +715,7 @@ class Albumentations:
         """Initialize the transform object for YOLO bbox formatted params."""
         self.p = p
         self.transform = None
-        self.transform_aa = None
+        # self.transform_aa = None
         prefix = colorstr('albumentations: ')
         try:
             import albumentations as A
@@ -687,7 +724,7 @@ class Albumentations:
 
             # T_always_app = [
             # #   A.GaussNoise(var_limit=(0.0021*255, 0.0023*255), mean=0, per_channel=False, always_apply=True),
-            #   A.GaussianBlur(blur_limit=(3, 7), sigma_limit=(10, 255), always_apply=True),
+            #   A.GaussianBlur(blur_limit=21, sigma_limit=(10, 255), always_apply=True),
             #   A.GaussNoise(var_limit=(0.1*255, 1.0*255), mean=0, per_channel=True, always_apply=True)
             # ]
             # self.transform_aa = A.Compose(T_always_app)
@@ -804,6 +841,8 @@ class Format:
 def v8_transforms(dataset, imgsz, hyp, stretch=False):
     """Convert images to a size suitable for YOLOv8 training."""
     pre_transform = Compose([
+        RandomNoises(p=1.0),
+        SensorNoise(p=1.0),
         Mosaic(dataset, imgsz=imgsz, p=hyp.mosaic),
         CopyPaste(p=hyp.copy_paste),
         RandomPerspective(
@@ -826,7 +865,6 @@ def v8_transforms(dataset, imgsz, hyp, stretch=False):
     return Compose([
         pre_transform,
         MixUp(dataset, pre_transform=pre_transform, p=hyp.mixup),
-        SensorNoise(p=1.0),
         Albumentations(p=1.0),
         RandomHSV(hgain=hyp.hsv_h, sgain=hyp.hsv_s, vgain=hyp.hsv_v),
         RandomFlip(direction='vertical', p=hyp.flipud),
--- ultralytics/yolo/data/custom_noise.py
+++ ultralytics/yolo/data/custom_noise.py
@@ -1,9 +1,182 @@
 import numpy as np
+import random
 import cv2
 from scipy.stats import tukeylambda
+import albumentations as Alb
 
 
-def sensor_noise(image, sensor, bit_depth=8, integration_time=0.1, use_PNRU=False, use_blur=False):
+def get_image_bit_depth(image):
+    if image is None:
+        raise ValueError("Failed to read the image.")
+    bit_depth = image.dtype.itemsize * 8
+    return bit_depth
+
+
+def apply_random_flare(image):
+
+    bit_depth = get_image_bit_depth(image)
+
+    flag_GRAYtoRGB = False
+    flag_SCALE = False
+
+    if bit_depth != 8:
+        # scale to 8 bit
+        flag_SCALE = True
+        scaling = ((2 ** 8) / (2 ** bit_depth))
+        image = image * scaling
+
+    elif len(image.shape) == 2:
+        flag_GRAYtoRGB = True
+        image = cv2.cvtColor(image, cv2.COLOR_GRAY2RGB)
+
+    # get flare center point
+    flare_center = get_flare_center(image, thresh=240)
+
+    if not np.isnan(flare_center).any():
+
+        # normalize flare center coordinate
+        center_norm_x = flare_center[0]/image.shape[1]
+        center_norm_y = flare_center[1] / image.shape[0]
+
+        x_min = np.max([0, center_norm_x-0.01])
+        y_min = np.max([0, center_norm_y-0.01])
+        x_max = np.min([1, center_norm_x+0.01])
+        y_max = np.min([1, center_norm_y+0.01])
+
+        img_diag_size = np.hypot(image.shape[0], image.shape[1])
+        radius_limit_lower = int(img_diag_size*0.05)
+        radius_limit_upper = int(img_diag_size*0.35)
+
+        # apply random flare
+        transf_flare = Alb.RandomSunFlare(
+            flare_roi=(x_min, y_min, x_max, y_max),
+            angle_lower=0,
+            angle_upper=1,
+            num_flare_circles_lower=0,
+            num_flare_circles_upper=3,
+            src_radius=np.random.randint(radius_limit_lower, radius_limit_upper),
+            always_apply=True,
+            p=0.5
+        )
+
+        output_transf = transf_flare(image=image)
+
+        flare_img = output_transf['image']
+    
+    else:
+        # case in which flare should not be applied
+        flare_img = image.copy()
+
+    # blur the output image to smooth the flare edges
+    transf_blur = Alb.OneOf([
+        Alb.MotionBlur(blur_limit=11, p=0.3),
+        Alb.GaussianBlur(blur_limit=11, sigma_limit=(10, 150), p=0.4),
+        Alb.GlassBlur(sigma=1., p=0.3)
+    ], p=1)
+
+    output_blur = transf_blur(image=flare_img)
+
+    output_img = output_blur['image']
+
+    if bit_depth == 8:
+        output_img = output_img.astype('uint8')
+    else:
+        output_img = output_img.astype('uint')
+
+    if flag_GRAYtoRGB is True:
+        output_img = cv2.cvtColor(output_img, cv2.COLOR_RGB2GRAY)
+
+    if flag_SCALE is True:
+        output_img = output_img/scaling
+
+    return output_img
+
+
+def get_flare_center(image, thresh=240):
+
+    bit_depth = get_image_bit_depth(image)
+
+    if bit_depth != 8:
+        # scale to 8 bit
+        image = image * ((2 ** 8) / (2 ** bit_depth))
+
+    # get the grayscale image if the image is RGB
+    if len(image.shape) == 3:
+        image_gs = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    else:
+        image_gs = image.copy()
+
+    # threshold the image to get a binary mask to extract only brightest portions
+    _, mask = cv2.threshold(src=image_gs.astype('float64'), thresh=thresh, maxval=1, type=cv2.THRESH_BINARY)
+
+    flare_center = find_random_extpoint_from_mask(image_gs, mask)
+
+    return flare_center
+
+
+def find_random_extpoint_from_mask(image, mask):
+
+    if len(image.shape) == 3:
+        image_gs = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    else:
+        image_gs = image.copy()
+
+    # Get the contours of the binary image
+    contours, _ = cv2.findContours(mask.astype('uint8'), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Initialize the variable that will contain the blob with the highest amount of high intensity pixels
+    max_blob = None
+    max_blob_area = 0
+
+    # Get the blob with the highest number of high intensity pixels
+    for contour in contours:
+        area = cv2.contourArea(contour)
+        if area > max_blob_area:
+            max_blob_area = area
+            max_blob = contour
+
+    if max_blob is None:
+        return np.array([np.nan, np.nan])
+
+    # Get the perimeter of the blob
+    perimeter = cv2.arcLength(max_blob, True)
+
+    # Get extreme points on the perimeter of the blob 
+    perimeter_points = cv2.approxPolyDP(max_blob, 0.02 * perimeter, True)
+
+    iter_num = 0
+    flag_WHITE = False
+
+    while flag_WHITE is False and iter_num < 1000:
+
+        iter_num += 1
+
+        # Get the indexes of two random points
+        idxs = random.sample(list(np.arange(len(perimeter_points))), 2)
+
+        # Compute point to point distance and pointing unit vector 
+        dist = np.linalg.norm(perimeter_points[idxs[0]] - perimeter_points[idxs[1]])
+        if dist == 0:
+            continue
+
+        dir = (perimeter_points[idxs[0]] - perimeter_points[idxs[1]]) / dist
+
+        # Define a random scale for the displacement
+        random_scale = np.random.randint(0, dist + 0.1)
+
+        # Extract a random point
+        random_point = perimeter_points[idxs[1]] + random_scale * dir
+        random_point = np.squeeze(random_point)
+
+        # Check that the selected point is over a white region
+        if image_gs[random_point.astype('int')[1], random_point.astype('int')[0]] > 200:
+            flag_WHITE = True
+            break
+
+    return random_point
+
+
+def sensor_noise(image, sensor, integration_time=0.1, use_PNRU=False, use_blur=False, use_bloom=False, row_noise=False):
     """
     Function to apply sensor noise to the input image
 
@@ -21,6 +194,8 @@ def sensor_noise(image, sensor, bit_depth=8, integration_time=0.1, use_PNRU=Fals
 
     """
 
+    bit_depth = get_image_bit_depth(image)
+
     flag_RGBtoGRAY = False
     flag_GRAYtoRGB = False
 
@@ -31,6 +206,10 @@ def sensor_noise(image, sensor, bit_depth=8, integration_time=0.1, use_PNRU=Fals
     elif len(image.shape) == 2 and sensor.is_RGB is True:
         flag_GRAYtoRGB = True
         image = cv2.cvtColor(image, cv2.COLOR_GRAY2RGB)
+
+    # apply bloom if required
+    if use_bloom:
+        image = do_blooming(image, bloom_thresh=sensor.bloom_thresh)
 
     # Apply blur to the input image if required
     if use_blur:
@@ -95,8 +274,15 @@ def sensor_noise(image, sensor, bit_depth=8, integration_time=0.1, use_PNRU=Fals
     else:
         I_read_e = tukeylambda.rvs(lam=sensor.read_shape, loc=sensor.read_mean, scale=sensor.std_read, size=I_e.shape)
 
-    # Step 7) Get the total electrons to form the noised image and convert to DN
-    I_total = np.rint(I_light_e + I_dc_e + I_read_e)
+    # Step 7) Get the row noise if required
+    if row_noise is True:
+        I_row = np.random.normal(loc=0, scale=sensor.row_std, size=(I_light_e.shape[0], 1))
+        I_row_mat = np.zeros(I_light_e.shape[:2]) + I_row
+    else:
+        I_row_mat = np.zeros(I_light_e.shape)
+
+    # Step 8) Get the total electrons to form the noised image and convert to DN
+    I_total = np.rint(I_light_e + I_dc_e + I_read_e + I_row_mat)
     I_total_e = np.clip(I_total, 0, sensor.full_well)
     I_dn = I_total_e / sensor.G
 
@@ -106,7 +292,7 @@ def sensor_noise(image, sensor, bit_depth=8, integration_time=0.1, use_PNRU=Fals
     else:
         I_dn_norm = I_dn.copy()
 
-    # Step 8) Apply quantization error
+    # Step 9) Apply quantization error
     I_q = np.random.uniform(low=-0.5, high=0.5, size=I_dn_norm.shape)
     I_dn_q = np.clip(np.rint(I_dn_norm + I_q), 0, (2 ** bit_depth - 1))
 
@@ -121,6 +307,69 @@ def sensor_noise(image, sensor, bit_depth=8, integration_time=0.1, use_PNRU=Fals
         I_dn_q = cv2.cvtColor(I_dn_q, cv2.COLOR_GRAY2RGB)
 
     return I_dn_q
+
+
+def do_blooming(image, bloom_thresh=200):
+    """
+    Function to add blooming to an image in post processing.
+
+    :param image:
+    :param bloom_thresh:
+    :return:
+    """
+
+    bit_depth = get_image_bit_depth(image)
+    flag_scale = False
+
+    if bit_depth != 8:
+        # scale to 8 bit
+        flag_scale = True
+        image = image * ((2**8)/(2**bit_depth))
+
+    flag_RGBtoGRAY = False
+
+    # get the grayscale image if the image is RGB
+    if len(image.shape) == 3:
+        image_gs = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+        flag_RGBtoGRAY = True
+    else:
+        image_gs = image.copy()
+
+    # threshold the image to get a binary mask to extract only brightest portions
+    _, mask = cv2.threshold(src=image_gs.astype('float64'), thresh=bloom_thresh, maxval=1, type=cv2.THRESH_BINARY)
+
+    # get the masked image with only brightest pixels
+    bright_image = np.where(mask, image_gs, 0)
+
+    # blur the mask and the bright image
+    mask_blurred = cv2.GaussianBlur(mask, ksize=(31, 31), sigmaX=40., sigmaY=0.)
+
+    bright_image_blurred = cv2.GaussianBlur(bright_image.astype('float64'), ksize=(31, 31), sigmaX=40., sigmaY=0.)
+
+    for i in range(4):
+        # blur the mask and the bright image
+        mask_blurred = cv2.GaussianBlur(mask_blurred, ksize=(31, 31), sigmaX=40., sigmaY=0.)
+
+        bright_image_blurred = cv2.GaussianBlur(bright_image_blurred.astype('float64'), ksize=(31, 31), sigmaX=40., sigmaY=0.)
+
+    # weights for blending
+    blend_weight = np.where(bright_image_blurred > image_gs, 1, 0)
+    blend_weight = cv2.GaussianBlur(blend_weight.astype('float64'), ksize=(31, 31), sigmaX=40., sigmaY=0.)
+
+    # get the bloomed image by using the normed mask as weight
+    bloomed_img = (1-blend_weight)*image_gs + blend_weight*bright_image_blurred
+
+    # clip the bloomed_image to the range
+    bloomed_img = np.clip(bloomed_img, 0, 2**bit_depth - 1)
+
+    if flag_RGBtoGRAY is True:
+        # use uint8 because it is the only format available for grayscale images color conversion
+        bloomed_img = cv2.cvtColor(bloomed_img.astype('uint8'), cv2.COLOR_GRAY2RGB)
+
+    if flag_scale is True:
+        bloomed_img = bloomed_img * ((2**bit_depth)/(2**8))
+
+    return bloomed_img
 
 
 class Sensor_VIS:
@@ -166,7 +415,8 @@ class Sensor_VIS:
 
         return
 
-    def noise_parameters(self, PRNU=0.01, dark_current=750.0, read_shape=0.14, read_mean=0.0, std_read=40.0):
+    def noise_parameters(self, PRNU=0.01, dark_current=750.0, read_shape=0.14, read_mean=0.0, std_read=40.0,
+                         bloom_thresh=200, row_std=20.):
         """
         Sensor Noise Parameters initialization.
 
@@ -175,6 +425,11 @@ class Sensor_VIS:
         :param read_shape: [-] - Tukey-Lambda shape parameter. If equal to 0.14 it approximates a Normal distribution.
         :param read_mean: [-] - Mean (or "loc") value for Tukey-Lambda distribution.
         :param std_read: [-] - Standard deviation (or "scale") value for Tukey-Lambda distribution.
+        :param bloom_thresh: [-] - Threshold intensity value over which the pixel can generate blooming effect.
+        :param row_std: [-] - Standard deviation (or "scale") value for row noise intensity fluctuation.
+
+
+        Note: to have a given offset X in pixels values due to read noise, set read_mean as X*sensor_Gain.
 
         :return: None.
         """
@@ -195,6 +450,9 @@ class Sensor_VIS:
                 self.read_mean = read_mean
 
         self.std_read = std_read
+
+        self.bloom_thresh = bloom_thresh
+        self.row_std = row_std
 
         return
 
